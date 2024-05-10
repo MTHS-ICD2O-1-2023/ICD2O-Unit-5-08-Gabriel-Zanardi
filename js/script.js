7da9ce30ea3516802a6ba00c35f51b450ba68c90
Gabriel Zanardi Nogueira <159185296+Gabriel-Zanardi@users.noreply.github.com>
2024-05-10 18:02:12 +0000

    init

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Gabriel
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function myButtonClicked() {
+
+  //input
+  const dayOfWeek = document.getElementById("day-of-week").innerHTML
+  const personAge = document.getElementById("person-age").innerHTML
+
+  //process
+  while {
+
+  }
+}
